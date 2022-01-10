d60dfaf8319e7aade9b9cfe9ec473a8055acb3a6
srinivas <chokkalla.srinivas>
2022-01-10 14:39:10 +0530

    srinivas123

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgswitchComponent } from './ngswitch/ngswitch.component';
+import { NgForComponent } from './ng-for/ng-for.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgswitchComponent,
+    NgForComponent
   ],
   imports: [
     BrowserModule,
